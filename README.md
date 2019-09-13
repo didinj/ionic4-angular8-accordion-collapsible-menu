f596dfea42ce844580be8332392b935e96bbcf8a
didin jamaludin <didinj@gmail.com>
2019-09-13 17:40:00 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ionic 4 Tutorial: Angular 8 Multilevel Accordion Menu Example
+
+This source code is part of [Ionic 4 Tutorial: Angular 8 Multilevel Accordion Menu Example]().
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -10,25 +11,35 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
-  ];
+  // public appPages = [
+  //   {
+  //     title: 'Home',
+  //     url: '/home',
+  //     icon: 'home'
+  //   },
+  //   {
+  //     title: 'List',
+  //     url: '/list',
+  //     icon: 'list'
+  //   }
+  // ];
+
+  public appPages: any;
+  showLevel1 = null;
+  showLevel2 = null;
+  showLevel3 = null;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private data: DataService
   ) {
     this.initializeApp();
+    this.data.getData().subscribe((resp) => {
+      console.log(resp);
+      this.appPages = resp;
+    });
   }
 
   initializeApp() {
@@ -36,5 +47,49 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  clearLevel() {
+    this.showLevel1 = null;
+    this.showLevel2 = null;
+    this.showLevel3 = null;
+  }
+
+  toggleLevel1(idx: string) {
+    if (this.isLevel1Shown(idx)) {
+      this.showLevel1 = null;
+    } else {
+      this.showLevel1 = idx;
+    }
+  }
+
+  toggleLevel2(idx: string) {
+    if (this.isLevel2Shown(idx)) {
+      this.showLevel2 = null;
+    } else {
+      this.showLevel1 = idx;
+      this.showLevel2 = idx;
+    }
+  }
+
+  toggleLevel3(idx: string) {
+    if (this.isLevel3Shown(idx)) {
+      this.showLevel3 = null;
+    } else {
+      this.showLevel2 = idx;
+      this.showLevel3 = idx;
+    }
+  }
+
+  isLevel1Shown(idx: string) {
+    return this.showLevel1 === idx;
+  }
+
+  isLevel2Shown(idx: string) {
+    return this.showLevel2 === idx;
+  }
+
+  isLevel3Shown(idx: string) {
+    return this.showLevel3 === idx;
   }
 }
